0deedbfbbffd855c8a90106ab5e699b070c933f9
Chandana N <chandana3130@gmail.com>
2026-03-17 16:17:45 +0530

    Change IMAP login credentials
    Updated login credentials for IMAP connection.

--- imap_debug.py
+++ imap_debug.py
@@ -1,4 +1,4 @@
 import imaplib
 imap = imaplib.IMAP4_SSL("imap.zoho.in", 993)
-imap.login("chandana3118@zohomail.in", "80RDhkmM7bSC")
+imap.login("--@zohomail.in", "16 letters")#app pass
 print("Success!")
